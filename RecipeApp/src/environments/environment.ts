c551bcc4a3e9794412603101951503a15ef198dc
David East <daithiwest@gmail.com>
2017-12-07 17:40:01 +0000

    environment keys added

--- RecipeApp/src/environments/environment.ts
+++ RecipeApp/src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: "AIzaSyBlJ0H4hG-F9EaF7SKylzG169c8hVc4B0w",
+    authDomain: "recipeapp-59906.firebaseapp.com",
+    databaseURL: "https://recipeapp-59906.firebaseio.com",
+    projectId: "recipeapp-59906",
+    storageBucket: "recipeapp-59906.appspot.com",
+    messagingSenderId: "159345926327"
   }
 };
